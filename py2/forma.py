c13a6009b8c65aabd39effa1d612f23dc7ebe92e
Bruno Weber <128698720+BrunoErd@users.noreply.github.com>
2024-05-17 12:07:12 -0300

    forma.py

---
+++ py2/forma.py
@@ -0,0 +1,23 @@
+class Forma():
+    def area(self):
+        pass
+
+class Quadrado(Forma):
+    def __init__(self, lado):
+        self.lado = lado
+    
+    def area(self):
+        return self.lado ** 2
+    
+quadrado = Quadrado(12)
+print(quadrado.area())
+
+class Circulo(Forma):
+    def __init__(self, raio):
+        self.raio = raio
+
+    def area(self):
+        return 3.14 * self.raio ** 2
+
+circulo = Circulo(12)
+print(circulo.area())
